1b1e46bc2a8053441a563ecb899bb83712712ed1
TTXXSS <1374230455@qq.com>
2019-08-10 13:13:37 +0800

    add:add new test method

--- src/test/java/PockerTest.java
+++ src/test/java/PockerTest.java
@@ -9,7 +9,24 @@ public class PockerTest {
         String s1="3a";
         String s2="5a";
         Pocker pocker = new Pocker(s1,s2);
-        String largerResult = pocker.compareLager(s1,s2);
+        String largerResult = pocker.compareLager(pocker.getPocker1(),pocker.getPocker2());
         assertSame(largerResult,s2);
+    }
+    @Test
+    public void should_return_2c_3h_4s_8c_ah_when_call_compareLarger_given_2h_3d_5s_9c_kd_and_2c_3h_4s_8c_ah(){
+        String s1="2h 3d 5s 9c kd";
+        String s2="2c 3h 4s 8c ah";
+        Pocker pocker = new Pocker(s1,s2);
+        String largerResult = pocker.compareLager(pocker.getPocker1(),pocker.getPocker2());
+        assertSame(largerResult,s2);
+    }
+    @Test
+    public void should_return_2h_2d_5s_9c_kd_when_call_compareLarger_given_2h_2d_5s_9c_kd_and_2c_3h_4s_8c_ah() {
+        String s1 = "2h 2d 5s 9c kd";
+        String s2 = "2c 3h 4s 8c ah";
+        Pocker pocker = new Pocker(s1,s2);
+        String largerResult = pocker.compareLager(pocker.getPocker1(),pocker.getPocker2());
+        assertSame(largerResult,s1);
+
     }
 }
