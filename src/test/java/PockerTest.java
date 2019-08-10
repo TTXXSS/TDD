998a68bd6ee7ad510c346f8c496117a8c89dfb9f
TTXXSS <1374230455@qq.com>
2019-08-10 15:36:38 +0800

    refactor:refactor method getCountSameElement

--- src/test/java/PockerTest.java
+++ src/test/java/PockerTest.java
@@ -38,4 +38,12 @@ public class PockerTest {
         assertSame(largerResult,s1);
 
     }
+    @Test
+    public void should_return_2h_2d_2s_9c_kd_when_call_compareLarger_given_2h_2d_2s_9c_kd_and_2c_2d_5s_9c_kd() {
+        String s1 = "2h 2d 2s 9c kd";
+        String s2 = "2c 2d 5s 9c kd";
+        Pocker pocker = new Pocker(s1,s2);
+        String largerResult = pocker.compareLager(pocker.getPocker1(),pocker.getPocker2());
+        assertSame(largerResult,s1);
+    }
 }
